49a7a6c51200bb8118d051b922d4186055d3b1e6
Joseph Blessingh <fruitfuljoseph@gmail.com>
2020-10-02 00:29:09 +0530

    Merge Audio Files Functionality added

--- task2.py
+++ task2.py
@@ -3,6 +3,8 @@ from pandas import read_csv
 
 import os
 import sys
+import glob
+import re
 
 class Task2:
 
@@ -10,6 +12,7 @@ class Task2:
 		self.df = df
 		self.file_name = file_name
 		self.speakers = []
+		self.speaker_set = ()
 
 	def merge_timestamp(self):
 		df_length = len(df.index) 
@@ -19,6 +22,7 @@ class Task2:
 		start_list = df['start_time'].values.tolist()
 		end_list = df['end_time'].values.tolist()
 
+		self.speaker_set = sorted(list(set(speaker_list)))
 
 		for i in range(0,len(speaker_list)):
 			current_row = []						
@@ -50,9 +54,40 @@ class Task2:
 	def trim(self):
 		cursor = 0
 		for speaker in self.speakers:
-			command = f"ffmpeg -y -i {self.file_name} -ss {speaker[1]} -to {speaker[2]} -c:v copy -c:a copy {speaker[0]+str(cursor)+'.wav'}"
-			os.system(command)
+			new_file = speaker[0]+str(cursor)+'.wav'
+			command = f"ffmpeg -y -i {self.file_name} -ss {speaker[1]} -to {speaker[2]} -c:v copy -c:a copy {new_file}"
+			try:
+				os.system(command)
+				content = "file '{}'".format(new_file)
+
+			except Exception as err:
+				print(f'Error occurred: {err}')
+
 			cursor = cursor + 1
+
+	def generate_files(self):
+		txt_files = []
+		for i in range(len(self.speaker_set)):
+			fileName = '{}.txt'.format(self.speaker_set[i])
+			with open(fileName,'a+') as f:
+				txt_files.append(fileName)
+				wavFiles = glob.glob('{}*.wav'.format(self.speaker_set[i]))
+
+				convert = lambda text: int(text) if text.isdigit() else text
+				alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+				wavFiles = sorted(wavFiles,key=alphanum_key)
+
+				for wavFile in wavFiles:
+					f.write('file \'{}\'\n'.format(wavFile))
+
+		self.merge_audio(txt_files)
+
+
+	def merge_audio(self, txt_files):
+		for txt_file in txt_files:
+			command = f"ffmpeg -y -f concat -i {txt_file} -c copy {txt_file[:-4]}.wav"
+			os.system(command)
+
 
 
 if __name__ == "__main__":
@@ -65,3 +100,4 @@ if __name__ == "__main__":
 	obj = Task2(df,file_name)
 	obj.merge_timestamp()
 	obj.trim()
+	obj.generate_files()
